085e7ccfba2e67ca5ecf7fab16c011442bb4b510
purdyjen <purdyjen@me.com>
2020-01-21 17:25:11 -0600

    routine push

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function() {  //doc ready
 
-  var firebaseConfig = {
+
+  var config = {
     apiKey: "AIzaSyBqSc9Clq98i4lKcSOFvL0kku_LIGu_qcs",
     authDomain: "rock-paper-scissors-5b1e2.firebaseapp.com",
     databaseURL: "https://rock-paper-scissors-5b1e2.firebaseio.com",
@@ -12,13 +13,53 @@ $(document).ready(function() {  //doc ready
   };
 
   // Initialize Firebase
-  firebase.initializeApp(firebaseConfig);
+  firebase.initializeApp(config);
   
 // Create a variable to reference the database.
 var database = firebase.database();
+var connectionsRef = database.ref("/connections");
+var connectedRef = database.ref(".info/connected"); 
+
+connectedRef.on("value", function(snap) {
+  if (snap.val()) {
+    //add user to connected list
+    var connected = connectionsRef.push(true)
+    //remove user when they disconnect
+    connected.onDisconnect().remove();
+  }
+});
+
+
+connectionsRef.on("value", function(snap) {
+  // find spot on page where you want to show the number of connected viewers. 
+  //Select it with JQ, then display with text the number of connected users that comes back from the snapshot
+ });
+
+/// part of the getInGame() function
+function getInGame() {
+
+  if (currentPlayers < 2) {
+    if (playerOneExists) {
+      playerNum = 2;
+    } else {
+      playerNum = 1;
+    }
+  };
+
+  // Creates key based on assigned player number
+  playerRef = database.ref("/players/" + playerNum);
+  // Creates player object. 'choice' is unnecessary here, but I left it in to be as complete as possible
+  playerRef.set({
+    name: username,
+    wins: 0,
+    losses: 0,
+    choice: null
+  });
+}
+
 
 $("#player-one-submit").on("click", function (){
-  playerOneSelection = $('input[name=choiceRadios]:checked').val();
+  playerOneChoice = $('input[name=choiceRadios]:checked').val();
   console.log(selection);
 });
 
